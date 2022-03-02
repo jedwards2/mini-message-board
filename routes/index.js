52e628cc558318508a9e344c26ba194ecdd5c169
Jack <jack.morgan.edwards@gmail.com>
2022-03-02 15:43:23 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express", messages });
+});
+
+router.post("/new", (req, res, next) => {
+  console.log(req.body);
+  let messageText = req.body.authorInput;
+  let messageUser = req.body.messageInput;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
+module.exports = router;
